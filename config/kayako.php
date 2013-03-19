ee878a9c2f29de6efafc16ed8d99fab5037a98b8
Daniel Sposito <dsposito@static.com>
2013-03-19 14:16:35 -0500

    Added default kayako package config file.

---
+++ config/kayako.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+	/**
+	 * url - The url to the Kayako install.
+	 */
+	'url' => 'https://your-url.com/support/api/index.php',
+	
+	/**
+	 * api_url - The path to your Kayako install's API.
+	 */
+	'api_path' => '/support/api',
+	
+	/**
+	 * api_key - The unique key that identifies the application.
+	 */
+	'api_key' => 'your_api_key',
+	
+	/**
+	 * secret_key - The secret key used to sign all the requests dispatched to Kayako.
+	 */
+	'secret_key' => 'your_secret_key',
+);
